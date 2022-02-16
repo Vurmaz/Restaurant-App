c1720297315e99832a177d269520643ba38b7639
Vurmaz <sadikvurmaz@gmail.com>
2022-02-17 00:34:45 +0300

    changed resposive design

--- src/menu.js
+++ src/menu.js
@@ -8,10 +8,10 @@ let array = []
 		}
 	}
 (function createMenuItem() {
-	const burger1 = new menu('Quatro Burger','15$','Our Special Burger with Two Burgers','./images/burger1.jpg')
+	const burger1 = new menu('Quatro Burger','15$',' Special Quote Burger with Two Double Burgers','./images/burger1.jpg')
 	const burger2 = new menu('Double Cheese Burger','10$','Delicious Beef and Double Cheese','./images/burger2.jpg')
 	const burger3 = new menu('Fresh Burger','12$','Beef with Lettuce Mayonnaise Tomato and Cheese','./images/burger3.jpg')
-	const burger4 = new menu('Twin Burgers','25$','Two Burgers with Special Sauces','./images/burger4.jpg')
+	const burger4 = new menu('Twin Burgers','25$','Two Burgers with Special Twin Sauces','./images/burger4.jpg')
 	array.push(burger1,burger2,burger3,burger4)
 })()
 function createMenu() {
